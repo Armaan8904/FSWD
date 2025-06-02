ce060f9a4aa2d4aab2edae900c55bcce1e7bcd48
Armaan8904 <armaansalam786@gmail.com>
2025-06-02 23:43:41 +0530

    Update App.js

--- resume/src/App.js
+++ resume/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   const handleDownload = () => {
-    window.location.href = 'https://fswd-07fo.onrender.com/resume';
+    window.location.href = 'https://fswd-724n.onrender.com';
   };
 
   return (
